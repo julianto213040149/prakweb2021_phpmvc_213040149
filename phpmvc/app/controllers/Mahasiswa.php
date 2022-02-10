b51fec00862de8c7106e1691d28d336401240c70
julianto213040149 <hardimasjulianto@gmail.com>
2022-02-11 06:29:22 +0700

    menyelesaikan video MVC bagian Model FIX

--- phpmvc/app/controllers/Mahasiswa.php
+++ phpmvc/app/controllers/Mahasiswa.php
@@ -4,8 +4,9 @@ class Mahasiswa extends controller {
     public function index()
     {
         $data['judul'] = 'Daftar Mahasiswa' ;
+        $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
         $this->view('Templates/header', $data);
-        $this->view('Mahasiswa/index');
+        $this->view('Mahasiswa/index' $data);
         $this->view('Templates/footer');
     }
 }
